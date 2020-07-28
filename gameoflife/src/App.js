5e4e573040450440b4e97f5ad167f6c59bdb0c47
svyatoslav-okshin <svyatokshin@gmail.com>
2020-07-28 15:13:50 -0400

    added the file structure

---
+++ gameoflife/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Game from "./components/Game";
+import Home from "./components/Home";
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/game">
+          <Game />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
